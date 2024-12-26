678ba9afd44c1144a091bbaaa1c58f69db04dbfb
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-12-26 13:55:57 +0200

    add thank you page

---
+++ offer/offer_pro_invest_v1/thankyou_page.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ПроИнвест</title>
+    <link rel="stylesheet" href="./css/thankyou_page.css" />
+  </head>
+  <body>
+    <header class="header">
+      <div class="header__logo">
+        <img src="./unnamed.webp" alt="Logo" class="header__logo-img" /><span
+          class="header__logo-text"
+        >
+          ПроИнвест</span
+        >
+      </div>
+    </header>
+    <main class="main">
+      <div class="main__header">
+        <h1>Ваша заявка отправлена</h1>
+        <h2>Менеджер скоро свяжется с вами, чтобы уточнить все детали</h2>
+      </div>
+      <div class="main__content">
+        <span>Что наши клиенты говорят о нас</span>
+      </div>
+      <div class="main__reviews">
+        <div class="main__review">
+          <p>
+            Отличное приложение для начинающих инвесторов! Удобный интерфейс и
+            искуственный интеллект помогли мне грамотно вложить средства.
+          </p>
+          <div class="main__review-avatar">
+            <img src="./images/avatars/Pic-1.png" alt="User" />
+            <div class="main__review-avatar-name">
+              <span>Сергей Кузнецов</span>
+              <img src="./images/stars.svg" alt="Rating" />
+            </div>
+          </div>
+        </div>
+        <div class="main__review">
+          <p>
+            Очень довольна этим приложением — все прозрачно и понятно. Уже вижу
+            первые результаты своих инвестиций!
+          </p>
+          <div class="main__review-avatar">
+            <img src="./images/avatars/Pic-2.png" alt="User" />
+            <div class="main__review-avatar-name">
+              <span>Мария Киселева</span>
+              <img src="./images/stars.svg" alt="Rating" />
+            </div>
+          </div>
+        </div>
+        <div class="main__review">
+          <p>
+            Искусственный интеллект отлично справляется с прогнозами, и
+            большинство моих сделок приносит прибыль – рекомендую!
+          </p>
+          <div class="main__review-avatar">
+            <img src="./images/avatars/Pic-3.png" alt="User" />
+            <div class="main__review-avatar-name">
+              <span>Алексей Смирнов</span>
+              <img src="./images/stars.svg" alt="Rating" />
+            </div>
+          </div>
+        </div>
+        <div class="main__review">
+          <p>
+            Понятный функционал и грамотный рынок-аналитик в одном приложении.
+            Точно рекомендую тем, кто хочет получать стабильный доход.
+          </p>
+          <div class="main__review-avatar">
+            <img src="./images/avatars/Pic-4.png" alt="User" />
+            <div class="main__review-avatar-name">
+              <span>Андрей Морозов</span>
+              <img src="./images/stars.svg" alt="Rating" />
+            </div>
+          </div>
+        </div>
+        <div class="main__review">
+          <p>
+            Никогда не думал, что разбираться в рынке может быть так просто. Это
+            приложение – находка для тех, кто хочет выйти на новый уровень
+            дохода.
+          </p>
+          <div class="main__review-avatar">
+            <img src="./images/avatars/Pic-5.png" alt="User" />
+            <div class="main__review-avatar-name">
+              <span>Михаил Соколов</span>
+              <img src="./images/stars.svg" alt="Rating" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </body>
+</html>
