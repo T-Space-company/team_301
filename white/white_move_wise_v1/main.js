1a7e21a689cc289cfd36a39ef791774a09a9e1ec
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-01-15 17:05:17 +0200

    add pages

--- white/white_move_wise_v1/main.js
+++ white/white_move_wise_v1/main.js
@@ -3,21 +3,21 @@ const dropdown = document.querySelector(".header__dropdown");
 const vnzh = document.getElementById("vnzh");
 const arrow = document.getElementById("vnzh-arrow");
 
-vnzh.addEventListener("click", (event) => {
-  event.preventDefault();
+// vnzh.addEventListener("click", (event) => {
+//     event.preventDefault();
 
-  const isOpen = dropdown.classList.contains("open");
+//   const isOpen = dropdown.classList.contains("open");
 
-  if (isOpen) {
-    dropdown.classList.remove("open");
-    vnzh.classList.remove("active");
-    arrow.classList.remove("active");
-  } else {
-    dropdown.classList.add("open");
-    vnzh.classList.add("active");
-    arrow.classList.add("active");
-  }
-});
+//   if (isOpen) {
+//     dropdown.classList.remove("open");
+//     vnzh.classList.remove("active");
+//     arrow.classList.remove("active");
+//   } else {
+//     dropdown.classList.add("open");
+//     vnzh.classList.add("active");
+//     arrow.classList.add("active");
+//   }
+// });
 
 document.addEventListener("click", (event) => {
   if (!menuItem.contains(event.target)) {
