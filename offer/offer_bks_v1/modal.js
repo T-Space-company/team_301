ba7c3f8d0213f800469b4e7dc09749c0ef5e121c
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-01-23 19:27:52 +0200

    fix/add all

--- offer/offer_bks_v1/modal.js
+++ offer/offer_bks_v1/modal.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
   const body = document.querySelector("body");
   const modal = document.getElementById("modal");
-  const btnForm = document.getElementById("button-form");
+  const signInBtn = document.getElementById("sign-in-button");
+  const btnModal = document.getElementById("button-form");
   const btnClose = document.getElementById("button-close");
+  const modalHeading = document.querySelector(".modal-form__heading");
+
+  if (window.matchMedia("(max-width: 1024px)").matches) {
+    modalHeading.textContent = "Откройте торговый счет";
+  }
 
   function openModal() {
     modal.style.display = "flex";
@@ -14,6 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     body.style.overflow = "visible";
   }
 
-  btnForm.addEventListener("click", openModal);
+  btnModal.addEventListener("click", openModal);
+  signInBtn.addEventListener("click", openModal);
   btnClose.addEventListener("click", closeModal);
 });
