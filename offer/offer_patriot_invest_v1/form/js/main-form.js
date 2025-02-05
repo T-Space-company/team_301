d270a88d3177721b319a14fc0b5197149d882efc
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-02-05 17:56:22 +0200

    fix/update

--- offer/offer_patriot_invest_v1/form/js/main-form.js
+++ offer/offer_patriot_invest_v1/form/js/main-form.js
@@ -30,10 +30,10 @@ const settingObjForm = {
       this.first_name = document.querySelector('input[name="name"]').value;
       this.last_name = document.querySelector('input[name="last_name"]').value;
       this.country_code = document.querySelector('input[name="code"]').value;
-      this.landing = "GzLanding";
+      this.landing = "PatriotLanding";
       this.source = sourceValue;
       this.utm_medium = buyerValue;
-      this.campaing_id = "Gaz";
+      this.campaing_id = "Sber";
     },
   },
 };
