fe482347a3ab03da5a6847cbd6b3a4c5880a0a8d
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-02-11 17:09:27 +0200

    add desktop/mobile / add slider

---
+++ offer/offer_synergetic_v1/assets/js/main.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const detailsElements = document.querySelectorAll("details");
+  const body = document.querySelector("body");
+  const modal = document.getElementById("modal");
+  const formButtons = document.querySelectorAll(".form-button");
+
+  formButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.remove("hidden");
+      body.style.overflow = "hidden";
+    });
+  });
+
+  window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+      modal.classList.add("hidden");
+      body.style.overflow = "auto";
+    }
+  });
+
+  detailsElements.forEach((detail) => {
+    const summary = detail.querySelector("summary");
+    const content = detail.querySelector(".details-content");
+
+    summary.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      if (!detail.open) {
+        content.style.display = "block";
+        let height = content.scrollHeight + "px";
+        content.style.maxHeight = height;
+
+        setTimeout(() => {
+          detail.open = true;
+        }, 100);
+      } else {
+        content.style.maxHeight = "0px";
+
+        setTimeout(() => {
+          detail.open = false;
+          content.style.display = "none";
+        }, 100);
+      }
+    });
+  });
+});
