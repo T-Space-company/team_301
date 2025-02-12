63971ec7960ca6d23cc4b1fd5ed91b8d8456f5e3
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-02-12 15:05:48 +0200

    add form/update all

--- offer/offer_sber_npf_v1/assets/js/main.js
+++ offer/offer_sber_npf_v1/assets/js/main.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const body = document.querySelector("body");
+  const formButton = document.querySelector(".form-button");
   const question1 = document.getElementById("question1");
   const question2 = document.getElementById("question2");
   const modal = document.getElementById("modal");
 
-  question1.classList.remove("hidden");
+  formButton.addEventListener("click", () => {
+    question1.classList.remove("hidden");
+    body.style.overflow = "hidden";
+  });
 
   document.querySelectorAll("#question1 .question-button").forEach((button) => {
     button.addEventListener("click", function () {
       if (this.textContent === "Нет") {
+        window.removeEventListener("beforeunload", trackClose);
         window.location.href = "err.html";
       } else {
         question1.classList.add("hidden");
@@ -19,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll("#question2 .question-button").forEach((button) => {
     button.addEventListener("click", function () {
       if (this.textContent === "до 18") {
+        window.removeEventListener("beforeunload", trackClose);
         window.location.href = "err.html";
       } else {
         question2.classList.add("hidden");
