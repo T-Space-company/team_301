3fcefeb97d68ebfe7dbf458eb2103c2b51039a6b
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-02-17 14:34:20 +0200

    update/fix all

--- offer/offer_patriot_quiz_v1/thanks2.php
+++ offer/offer_patriot_quiz_v1/thanks2.php
@@ -23,7 +23,7 @@
     <div class="div-block-58"><img src="images/call-center-operator-answering-clients-company-with-tech-support-1.png" loading="lazy" sizes="(max-width: 479px) 45vw, (max-width: 991px) 25vw, 30vw" srcset="images/call-center-operator-answering-clients-company-with-tech-support-1-p-500.png 500w, images/call-center-operator-answering-clients-company-with-tech-support-1-p-800.png 800w, images/call-center-operator-answering-clients-company-with-tech-support-1-p-1080.png 1080w, images/call-center-operator-answering-clients-company-with-tech-support-1.png 1500w" alt="" class="image-6">
       <div class="text-block-24-copy">Спасибо! <br>Ваша заявка получена!</div>
       
-      <p class="bottom-message"></p>
+      <p class="bottom-message" style="color: white;"></p>
     </div>
   </section>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=652f88b20bb4d9041f52bfc1" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
