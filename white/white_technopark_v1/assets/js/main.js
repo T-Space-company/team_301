984482b0c4fe4378d6c1ba5062d78ecf14a2ab11
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-02-26 13:11:08 +0200

    add full mobile/add desktop header

--- white/white_technopark_v1/assets/js/main.js
+++ white/white_technopark_v1/assets/js/main.js
@@ -1,4 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const supportButton = document.getElementById("support-button");
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      supportButton.classList.add("show");
+    } else {
+      supportButton.classList.remove("show");
+    }
+  });
+
   function updateImages() {
     const images = document.querySelectorAll(".responsive-img");
     const isMobile = window.matchMedia("(max-width: 1024px)").matches;
