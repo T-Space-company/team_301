0c729316e03165b772aeae238053eb3afb1d019f
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-03-19 11:28:21 +0200

    update/change id

--- offer/offer_patriot_invest_chat_v1/index.php
+++ offer/offer_patriot_invest_chat_v1/index.php
@@ -9,7 +9,7 @@ if (!isset($rawClick) && !isset($click)) {
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Патриот Инвестиции</title>
-    <link rel="icon" href="./img/icons/avatar.png" type="image/x-icon" />
+    <link rel="icon" href="./img/icons/avatar.svg" type="image/x-icon" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
