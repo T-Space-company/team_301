7a407e5e10b21c2feba39c3019582998b7a0024f
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-03-24 11:34:20 +0200

    add js/form/complete mobile

---
+++ white/white_wb_gifts_v1/assets/js/main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gifts = document.querySelectorAll(".gift");
+  const modal = document.getElementById("modal-form");
+
+  gifts.forEach((gift, index) => {
+    setTimeout(() => {
+      gift.classList.add("animate-in");
+    }, index * 300);
+
+    gift.addEventListener("click", openGift);
+
+    function openGift() {
+      gift.classList.remove("animate-in");
+
+      gift.classList.add("animate-shake");
+
+      setTimeout(() => {
+        gift.classList.remove("animate-shake");
+        gift.src = "./assets/img/gift_open.svg";
+
+        gift.classList.add("animate-open");
+
+        setTimeout(() => {
+          gift.classList.remove("animate-open");
+          modal.classList.remove("hidden");
+        }, 700);
+      }, 700);
+    }
+  });
+});
