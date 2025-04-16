9be223116d270aabc457618278ea76ca436012a0
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-04-16 16:36:43 +0300

    add template

---
+++ offer/offer_okx_quiz_v1/thanks2.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Спасибо за регистрацию!</title>
+    <link rel="stylesheet" href="./assets/css/normalize.css" />
+    <link rel="stylesheet" href="./assets/css/styles.css" />
+    <link rel="shortcut icon" href="./assets/img/fav.svg" type="image/x-icon" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <main class="main thanks">
+      <div class="screen main__thanks">
+        <div class="modal__content thanks">
+          <img src="./assets/img/logo_s.svg" alt="" />
+          <div class="main-image thanks">
+            <img src="./assets/img/phone.svg" alt="Background" id="phone" />
+          </div>
+          <div class="main-thankyou__block">
+            <h3>Спасибо за регистрацию</h3>
+            <span
+              >Наш менеджер свяжется с Вами удобным способом (Telegram, WhatsApp
+              или звонок) в течении 30 минут или в рабочее время!</span
+            >
+            <div class="timer thanks"></div>
+          </div>
+        </div>
+        <div class="main-thankyou__testimonials">
+          <h2 class="testimonials__heading">Отзывы о платформе:</h2>
+          <ul class="testimonials__list">
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-1.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Иван Сергеевич</span>
+                    <span>+7 (859) 656-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Самара</span>
+                  <span>34 года</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Я давно искал удобную платформу для пассивного заработка, и
+                наконец-то нашёл. Интерфейс очень понятный, регистрация простая,
+                а главное — всё работает стабильно. Доход приходит регулярно, а
+                вывод без лишних сложностей. Отличный сервис для тех, кто хочет
+                зарабатывать без глубоких знаний
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-2.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Мария Александровна</span>
+                    <span>+7 (875) 946-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Москва</span>
+                  <span>31 год</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Я не профессионал в этом, но всегда хотела попробовать.
+                Платформа позволила мне это сделать без необходимости изучать
+                тонны информации. Алгоритмы сами анализируют рынок, а я просто
+                получаю стабильный доход. Очень довольна, планирую увеличивать
+                вложения!
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-3.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Алексей Дмитриевич</span>
+                    <span>+7 (834) 156-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Саратов</span>
+                  <span>32 года</span>
+                </div>
+                <img src="./assets/img/avatars/ok.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                До <b>"БКС"</b> я пробовал другие инвестиционные сервисы, но
+                везде были либо высокие комиссии, либо непонятные схемы. Здесь
+                всё максимально прозрачно: понятная доходность, удобные
+                инструменты и самое главное — безопасность. Пользуюсь уже
+                несколько месяцев, и пока ни одного нарекания!
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-4.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Артем Евгеньевич</span>
+                    <span>+7 (848) 947-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Екатеринбург</span>
+                  <span>27 лет</span>
+                </div>
+                <img src="./assets/img/avatars/ok.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Военные всегда ценят надёжность, и <b>"БКС"</b> её
+                предоставляет. Здесь нет скрытых условий, всё предельно
+                прозрачно. Начал с небольшой суммы, протестировал вывод — всё
+                пришло без задержек. Теперь вкладываю больше и уверен, что
+                капитал работает эффективно. Советую тем, кто ищет стабильный
+                доход без лишних сложностей.
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-5.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Ольга Викторовна</span>
+                    <span>+7 (815) 173-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Санкт-Петербург</span>
+                  <span>28 лет</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Мне понравилось, что платформа открыто предоставляет всю
+                информацию о доходности, комиссиях и стратегиях. Нет скрытых
+                платежей или странных условий. Всё чётко и понятно. А ещё радует
+                быстрая поддержка, всегда готовы помочь и объяснить, если что-то
+                неясно.
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-6.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Николай Петрович</span>
+                    <span>+7 (874) 067-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Москва</span>
+                  <span>30 лет</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Раньше я пытался зарабатывать сам, но это отнимало кучу времени
+                и нервов. С <b>"БКС"</b> всё автоматизировано: платформа сама
+                анализирует рынок и делает всё за меня. Я просто выбираю
+                стратегию, а доход приходит регулярно. Для занятых людей —
+                идеальный вариант!
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-7.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Артур Тимурович</span>
+                    <span>+7 (861) 540-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Южно-Сахалинск</span>
+                  <span>36 лет</span>
+                </div>
+                <img src="./assets/img/avatars/ok.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Служба требует максимальной концентрации, поэтому времени на
+                анализ просто нет. <b>"БКС"</b> решает этот вопрос — всё
+                автоматизировано, доход приходит стабильно. Деньги работают, а я
+                продолжаю выполнять свои обязанности без лишних забот. Отличный
+                сервис, который можно доверить!
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-8.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Екатерина Андреевна</span>
+                    <span>+7 (863) 715-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Тюмень</span>
+                  <span>26 лет</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Главное, что меня интересует в любой платформе — это надёжность
+                и стабильность. <b>"БКС"</b> в этом плане идеален. Вывод денег
+                занимает всего пару минут, доход прогнозируемый, а риски
+                минимизированы. Уже рекомендовала друзьям!
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-9.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Василий Иванович</span>
+                    <span>+7 (818) 175-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Курск</span>
+                  <span>26 лет</span>
+                </div>
+                <img src="./assets/img/avatars/vk.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Служба не оставляет много времени на дополнительный заработок,
+                поэтому искал платформу, которая будет работать без моего
+                постоянного участия. <b>"БКС"</b> полностью оправдал ожидания!
+                Всё чётко, стабильно и без лишних рисков. Деньги работают, доход
+                поступает регулярно. Отличное решение для военных, кто хочет
+                создать пассивный источник заработка.
+              </p>
+            </li>
+            <li class="testimonials__item">
+              <div class="testimonials__info">
+                <div class="testimonials__name">
+                  <img src="./assets/img/avatars/t-10.png" alt="Image" />
+                  <div class="testimonials__contact">
+                    <span>Анна Константиновна</span>
+                    <span>+7 (847) 739-ХХ-ХХ</span>
+                  </div>
+                </div>
+                <div class="testimonials__city">
+                  <span>Воронеж</span>
+                  <span>29 лет</span>
+                </div>
+                <img src="./assets/img/avatars/ok.svg" alt="Icon" />
+              </div>
+              <p class="testimonials__text">
+                Очень нравится, что здесь используются передовые алгоритмы для
+                анализа рынка. Это позволяет минимизировать риски и увеличивать
+                доходность. Видно, что команда разработчиков действительно
+                заботится о качестве сервиса.
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </main>
+    <script src="./assets/js/timer.js"></script>
+  </body>
+</html>
