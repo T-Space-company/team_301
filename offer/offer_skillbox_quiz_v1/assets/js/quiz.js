caeabab69503c4df745df87acf6c8be96013c997
Mykhailo Konstantinov <mkonst10@gmail.com>
2025-05-14 14:32:41 +0300

    add template

---
+++ offer/offer_skillbox_quiz_v1/assets/js/quiz.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.querySelector("body");
+  const modal1 = document.getElementById("modal-1");
+  const modal2 = document.getElementById("modal-2");
+  const modal3 = document.getElementById("modal-3");
+  const btnYes1 = document.querySelectorAll(".modal__button--first.yes");
+  const btnYes2 = document.querySelector(".modal__button--second.yes");
+
+  localStorage.removeItem("answers");
+
+  function saveAnswer(question, answer) {
+    let answers = JSON.parse(localStorage.getItem("answers")) || [];
+    answers.push(`${question}: ${answer}`);
+    localStorage.setItem("answers", JSON.stringify(answers));
+  }
+
+  body.style.overflow = "hidden";
+  modal1.classList.remove("hidden");
+
+  btnYes1.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      saveAnswer("Сколько вам лет?", btn.textContent.trim());
+      modal1.classList.add("hidden");
+      modal2.classList.remove("hidden");
+    });
+  });
+
+  btnYes2.addEventListener("click", () => {
+    saveAnswer(
+      "Вы являетесь гражданином или резидентом Российской Федерации?",
+      "Да"
+    );
+    modal2.classList.add("hidden");
+    modal3.classList.remove("hidden");
+  });
+});
